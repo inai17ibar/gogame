cd94815b95fcd0ccfd7a142fbe0ae8064ee4fec0
Soichiro Inatani <inatani@SoichironoMacBook.local>
2021-02-07 19:35:09 +0900

    renamed email to name in user model field.

--- model.go
+++ model.go
@@ -2,7 +2,7 @@ package main
 
 type User struct {
 	ID        int    `json:"id"`
-	Email     string `json:"email"`
+	Name      string `json:"name"`
 	Password  string `json:"password"`
 	SessionId int    `json:"session_id"`
 }
